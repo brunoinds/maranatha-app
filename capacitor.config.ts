632ac2ce1726f9cf223a0e7b3ac0de4b6b103946
Bruno Freire <brunoqm99@gmail.com>
2023-10-21 19:40:52 -0500

    Updates iOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.maranathasender.maranathasender',
-  appName: 'maranatha-sender',
+  appName: 'Maranatha Sender',
   webDir: 'dist',
   server: {
     androidScheme: 'https'

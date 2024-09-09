8ff6b2453bb32c12e586643c346ea3e77c04038d
Bruno Freire <brunoqm99@gmail.com>
2024-09-09 18:32:36 -0400

    Update app.config.json version to 1.2.153

--- src/dialogs/EditInvoice/EditInvoice.vue
+++ src/dialogs/EditInvoice/EditInvoice.vue
@@ -621,6 +621,15 @@ const saveInvoice = async () => {
             pdf_base64: dynamicData.value.originalPdfBase64 == dynamicData.value.uploadedPdfBase64 ? null : dynamicData.value.uploadedPdfBase64
         } as unknown as IInvoice;
 
+        if (invoiceDocument.pdf_base64 || invoiceDocument.pdf){
+            invoiceDocument.image = null;
+            invoiceDocument.image_base64 = null;
+        }
+
+        if (invoiceDocument.image_base64 || invoiceDocument.image){
+            invoiceDocument.pdf = null;
+            invoiceDocument.pdf_base64 = null;
+        }
 
 
 
